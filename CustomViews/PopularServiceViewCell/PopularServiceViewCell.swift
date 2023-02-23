350b353a20c2a89cbcd274db282368f7171c9d92
sakibssamad <sssakib.info@gmail.com>
2023-02-23 17:40:01 +0600

    Quickreach initiated

---
+++ CustomViews/PopularServiceViewCell/PopularServiceViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  PopularServiceViewCell.swift
+//  TuttProj
+//
+//  Created by Era-MobileApp on 23/2/23.
+//
+
+import UIKit
+
+class PopularServiceViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var popularServiceImageView: UIImageView!
+    
+    
+    @IBOutlet weak var popularServiceTitleLbl: UILabel!
+    
+    func setup(popularService : PopularService){
+        
+    }
+
+}
